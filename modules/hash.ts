45dde00c997d0d863bf68ee1083502d437623e2f
LioX <mail@liox.tech>
2023-08-20 13:46:31 +0200

    Big update for the Setup Process

--- modules/hash.ts
+++ modules/hash.ts
@@ -1,5 +1,5 @@
 import crypto from "node:crypto";
 
 export function hash(input: string) {
-  return crypto.createHash("sha256").update(input).digest("hex");
+  return crypto.createHash("sha512").update(input).digest("hex");
 }
--- modules/rsa.ts
+++ modules/rsa.ts
@@ -1,22 +1,23 @@
 import OpenCrypto from "npm:deno-opencrypto";
 import os from "node:os";
 import path from "node:path";
-/* import fs from "node:fs"; */
+import fs from "node:fs";
 import ora from "npm:ora";
-import process from "node:process";
+import chalk from "npm:chalk";
 
-export function setup(password_hash: string | undefined) {
+export function setup(password_hash: string) {
   // deno-lint-ignore no-explicit-any
   const crypt = new (OpenCrypto as any as typeof OpenCrypto)();
 
-  console.log(password_hash);
-
   const app_folder = path.join(os.homedir(), "LxCrypt");
 
-  const spinner = ora("Initializing Certificates").start();
+  const spinner = ora(
+    "Initializing Certificates. This might take a while depending on your CPU!"
+  ).start();
 
   crypt
     .getRSAKeyPair(
+      /* 7680, */
       4096,
       "SHA-512",
       "RSA-OAEP",
@@ -24,43 +25,31 @@ export function setup(password_hash: string | undefined) {
       true
     )
     .then((keyPair: CryptoKeyPair) => {
-      crypt
-        .cryptoPrivateToPem(keyPair.privateKey)
-        .then((privatePem: string) => {
-          console.log(privatePem);
-        });
       crypt.cryptoPublicToPem(keyPair.publicKey).then((publicPem: string) => {
-        console.log(publicPem);
-        spinner.succeed("Initialization successful");
-        process.exit(1);
+        fs.writeFileSync(path.join(app_folder, "public_key.lxcf"), publicPem);
       });
+      crypt
+        .encryptPrivateKey(
+          keyPair.privateKey,
+          password_hash,
+          64000,
+          "SHA-512",
+          "AES-GCM",
+          256
+        )
+        .then((encryptedPrivateKey: string) => {
+          //note .lxcf = LxCrypt File
+          fs.writeFileSync(
+            path.join(app_folder, "private_key.lxcf"),
+            encryptedPrivateKey
+          );
+          spinner.succeed(chalk.green.bold("Initialization successful\n"));
+          console.log(
+            chalk.blue.bold(
+              "\nThe Setup is complete. Please reopen the Application\n\n"
+            )
+          );
+          Deno.exit(0);
+        });
     });
-
-  /* crypto.generateKeyPair(
-    "rsa",
-    {
-      modulusLength: 4096,
-      publicKeyEncoding: {
-        type: "spki",
-        format: "pem",
-      },
-      privateKeyEncoding: {
-        type: "pkcs8",
-        format: "pem",
-        cipher: "aes-256-cbc",
-        passphrase: password_hash,
-      },
-    },
-    (err: Error | null, public_key: string, private_key: string) => {
-      if (err) {
-        spinner.fail("Error!");
-        console.log(err);
-        process.exit(1);
-      }
-      //note .lxcf = LxCrypt File
-      fs.writeFileSync(path.join(app_folder, "private_key.lxcf"), private_key);
-      fs.writeFileSync(path.join(app_folder, "public_key.lxcf"), public_key);
-      spinner.succeed("Initialization successful");
-    }
-  ); */
 }
