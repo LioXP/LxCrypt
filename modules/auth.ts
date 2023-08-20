620b63084ab37c15b336507cf618a7b54ff16e8e
LioX <mail@liox.tech>
2023-08-20 14:40:11 +0200

    Update auth.ts

--- modules/auth.ts
+++ modules/auth.ts
@@ -2,6 +2,8 @@ import * as modules from "../module-manager.ts";
 import prompts from "npm:prompts";
 import chalk from "npm:chalk";
 import process from "node:process";
+import OpenCrypto from "npm:deno-opencrypto";
+
 export async function setup() {
   //todo explain
   const password = await prompts({
@@ -28,6 +30,9 @@ export async function setup() {
     } else {
       //todo explain
       console.log('You picked "no". Press any key to retry');
+      process.stdin.once("data", function () {
+        modules.setup.start();
+      });
     }
   } else {
     console.log(chalk.red("Passwords don't match!\n"));
@@ -36,5 +41,23 @@ export async function setup() {
       modules.setup.start();
     });
   }
+  return "";
 }
-export function login() {}
+export async function login() {
+  // deno-lint-ignore no-explicit-any
+  const crypt = new (OpenCrypto as any as typeof OpenCrypto)();
+  const password = await prompts({
+    type: "password",
+    name: "input",
+    message: "Please enter your password",
+  });
+  const password_hash = modules.hash.hash(password.input);
+
+  crypt
+    .decryptPrivateKey(encryptedPrivateKey, password_hash, options)
+    .then((decryptedPrivateKey: CryptoKey) => {
+      console.log(decryptedPrivateKey);
+    });
+
+  Deno.exit();
+}
