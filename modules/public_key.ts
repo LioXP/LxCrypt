82fda2dedc526d6041c159bf01f319fed9cb3e5e
LioX <mail@liox.tech>
2023-08-27 14:03:24 +0200

    Update public_key.ts

--- modules/public_key.ts
+++ modules/public_key.ts
@@ -1,20 +1,18 @@
 import * as modules from "../module-manager.ts";
-
-//! WEITER MACHEN
 export async function share(publicPem: string) {
   /* const body = `content=${publicPem}&expiry_days=365`; */
-  const body = `content=${publicPem}&expiry_days=1`; // note change expiry_days
+  const body = `content=${publicPem}&expiry_days=1`; //note change expiry_days
   const req = await fetch(modules.config.paste_api, {
     method: "POST",
     headers: {
       "User-Agent": modules.config.user_agent,
+      "Content-Type": "application/x-www-form-urlencoded",
     },
     body,
   });
-  if (req.status === 200) {
-    console.log("Working");
-    console.log(req);
+  if (req.status === 201) {
+    console.log("working");
   } else {
-    console.log("ERROR contacting " + modules.config.paste_api);
+    console.log("error");
   }
 }
